e96e9044121894feb85af312a23d6164fbb0d6c2
Xabier Matute Rusinol <xmatute-@c4r8s4.42urduliz.com>
2023-03-08 14:48:03 +0100

    añadodp argc error

--- philo/errors/argc_error.c
+++ philo/errors/argc_error.c
@@ -1,21 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   philosophers.h                                     :+:      :+:    :+:   */
+/*   argc_error.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: xmatute- <xmatute-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/03/08 13:06:58 by xmatute-          #+#    #+#             */
-/*   Updated: 2023/03/08 14:32:19 by xmatute-         ###   ########.fr       */
+/*   Created: 2023/03/08 14:09:11 by xmatute-          #+#    #+#             */
+/*   Updated: 2023/03/08 14:45:31 by xmatute-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef PHILOSOPHERS_H
-# define PHILOSOPHERS_H
+#include "../philosophers.h"
 
-# include <stdio.h>
-
-/* ------------------- ERRORS ---------------------*/
-int	argc_error(int argc);
-
-#endif
+int	argc_error(int argc)
+{
+	printf("❌Error: numero incorrecto de argumentos\n");
+	printf("  Has introducido %i\n", argc - 1);
+	printf("  El imput debe ser de la forma:\n");
+	printf("   ./philosopher number_of_philosophers time_to_die");
+	printf(" time_to_eat time_to_sleep");
+	printf(" [number_of_times_each_philosopher_must_eat\n");
+	return (argc);
+}
