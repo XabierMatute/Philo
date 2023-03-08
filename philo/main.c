5f405938021e21ff87cb0c8a4974c035c7591f7b
Xabier Matute Rusinol <xmatute-@c4r8s4.42urduliz.com>
2023-03-08 14:46:41 +0100

    seleccion de casos en el main

--- philo/main.c
+++ philo/main.c
@@ -6,15 +6,29 @@
 /*   By: xmatute- <xmatute-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/08 13:05:22 by xmatute-          #+#    #+#             */
-/*   Updated: 2023/03/08 13:24:01 by xmatute-         ###   ########.fr       */
+/*   Updated: 2023/03/08 14:44:16 by xmatute-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philosophers.h"
 
+int	case4(char const *argv[])
+{
+	return (0);
+}
+
+int	case5(char const *argv[])
+{
+	return (0);
+}
+
 int	main(int argc, char const *argv[])
 {
-	while (argc--)
-		printf("%i:%s\n", argc, argv[argc]);
+	if (argc == 5)
+		return (case4(argv));
+	else if (argc == 6)
+		return (case5(argv));
+	else
+		return (argc_error(argc));
 	return (0);
 }
