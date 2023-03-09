160b2f1d2ec6cb7214affa898fed669d77461a08
Xabier Matute Rusinol <xmatute-@c4r5s4.42urduliz.com>
2023-03-09 12:55:25 +0100

    philosophers.c

--- philo/philosophers.h
+++ philo/philosophers.h
@@ -6,7 +6,7 @@
 /*   By: xmatute- <xmatute-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/08 13:06:58 by xmatute-          #+#    #+#             */
-/*   Updated: 2023/03/08 18:53:48 by xmatute-         ###   ########.fr       */
+/*   Updated: 2023/03/09 12:53:37 by xmatute-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,25 @@
 
 # include <stdio.h>
 
-/* ------------------- checkinput ---------------------*/
-int	printcorrectinput(void);
-int	invalidinput(int argc, char const *argv[]);
-int	isnumeric2(int n, char const *s2[]);
-int	isinrange2(int n, char const *s2[]);
+int		philosophers(int arg[5]);
+
+/* ------------------- CHECKINPUT ---------------------*/
+int		printcorrectinput(void);
+int		invalidinput(int argc, char const *argv[]);
+int		isnumeric2(int n, char const *s2[]);
+int		isinrange2(int n, char const *s2[]);
+
+/* ------------------- PARSE ---------------------*/
+int		*parse(int argc, char const *argv[]);
+
+/* ------------------- UTILS ---------------------*/
+size_t	ft_strlen(const char *s);
+int		ft_strcmp(const char *str1, const char *str2);
+int		ft_atoi(const char *str);
 
 /* ------------------- ERRORS ---------------------*/
-int	argc_error(int argc);
-int	numeric_error(char const *s);
-int	range_error(char const *s);
+int		argc_error(int argc);
+int		numeric_error(char const *s);
+int		range_error(char const *s);
 
 #endif
