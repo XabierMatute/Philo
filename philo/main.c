98c0e72ae1a0623a994f47f7172e35b7627f176c
Xabier Matute Rusinol <xmatute-@c4r5s4.42urduliz.com>
2023-03-09 12:54:51 +0100

    nuevo main

--- philo/main.c
+++ philo/main.c
@@ -6,31 +6,17 @@
 /*   By: xmatute- <xmatute-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/08 13:05:22 by xmatute-          #+#    #+#             */
-/*   Updated: 2023/03/08 18:04:50 by xmatute-         ###   ########.fr       */
+/*   Updated: 2023/03/09 12:26:05 by xmatute-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philosophers.h"
 
-// int	case4(char const *argv[])
-// {
-// 	return (0);
-// }
-
-// int	case5(char const *argv[])
-// {
-// 	return (0);
-// }
-
 int	main(int argc, char const *argv[])
 {
 	if (invalidinput(argc, argv))
-		return(1);
-	// if (argc == 5)
-	// 	return (case4(argv));
-	// else if (argc == 6)
-	// 	return (case5(argv));
-	// else
-	// 	return (argc_error(argc));
+		return (1);
+	if (philosopher(parse(argc, argv)))
+		return (1);
 	return (0);
 }
